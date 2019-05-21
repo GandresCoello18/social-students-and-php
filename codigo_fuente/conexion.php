3444036584b6d5f610d94e584ab86728e6364f7a
GandresCoello18 <goyeselcoca@gmail.com>
2019-05-21 15:39:21 -0500

    conectando a la base de datos remota

--- codigo_fuente/conexion.php
+++ codigo_fuente/conexion.php
@@ -1,3 +1,3 @@
 <?php  
-    $conexion = new PDO('mysql:host=localhost;dbname=social-students', 'root','');
+    $conexion = new PDO('mysql:host=remotemysql.com;dbname=Higqey0L6O', 'Higqey0L6O','hSFOOBjPkM');
 ?>
